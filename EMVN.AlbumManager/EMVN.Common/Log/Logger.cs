580ee7f6b280a05049e316f4f781718431dfa525
Kim Hoang <kim.hoang@titandms.com.au>
2020-05-21 22:54:45 +0700

    commit missing

---
+++ EMVN.AlbumManager/EMVN.Common/Log/Logger.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMVN.Common.Log
+{
+    public class Logger
+    {
+        #region Singleton
+        private static object _sync = new object();
+        private static Logger _instance = null;
+        public static Logger Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_sync)
+                    {
+                        if (_instance == null)
+                            _instance = new Logger();
+                    }
+                }
+                return _instance;
+            }
+        }
+        private Logger()
+        {
+
+        }
+        #endregion
+
+        #region Fields
+        private readonly NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+        #endregion
+
+        #region Info
+        public void Info(string message)
+        {
+            _logger.Info(message);
+        }
+
+        public void Info(string message, params object[] args)
+        {
+            _logger.Info(message, args);
+        }
+        #endregion
+
+        #region Debug
+        public void Debug(string message)
+        {
+            _logger.Debug(message);
+        }
+
+        public void Debug(string message, params object[] args)
+        {
+            _logger.Debug(message, args);
+        }
+        #endregion
+
+        #region Warn
+        public void Warn(string message)
+        {
+            _logger.Warn(message);
+        }
+
+        public void Warn(string message, params object[] args)
+        {
+            _logger.Warn(message, args);
+        }
+        #endregion
+
+        #region Error
+        public void Error(string message)
+        {
+            _logger.Error(message);
+        }
+
+        public void Error(string message, params object[] args)
+        {
+            _logger.Error(message, args);
+        }
+
+        public void Error(Exception exception)
+        {
+            _logger.Error(exception);
+        }
+
+        public void Error(Exception exception, string message)
+        {
+            _logger.Error(exception, message);
+        }
+
+        public void Error(Exception exception, string message, params object[] args)
+        {
+            _logger.Error(exception, message, args);
+        }
+        #endregion
+
+        #region Fatal
+        public void Fatal(string message)
+        {
+            _logger.Fatal(message);
+        }
+
+        public void Fatal(string message, params object[] args)
+        {
+            _logger.Fatal(message, args);
+        }
+
+        public void Fatal(Exception exception)
+        {
+            _logger.Fatal(exception);
+        }
+
+        public void Fatal(Exception exception, string message)
+        {
+            _logger.Fatal(exception, message);
+        }
+
+        public void Fatal(Exception exception, string message, params object[] args)
+        {
+            _logger.Fatal(exception, message, args);
+        }
+        #endregion
+
+        #region Close
+        public void Close()
+        {
+            NLog.LogManager.Shutdown();
+        }
+        #endregion
+    }
+}
